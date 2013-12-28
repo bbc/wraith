14059c256b3dfb0d3750380f4da26a695d366e6b
DaveBlooman <david.blooman@gmail.com>
2013-12-28 23:54:28 +0000

    clean up

--- lib/wraith.rb
+++ lib/wraith.rb
@@ -1,4 +1,4 @@
-require "yaml"
+require 'yaml'
 
 class Wraith
   attr_accessor :config
@@ -56,25 +56,25 @@ class Wraith
   end
 
   def browser2
-     @config['device']['compare']
+    @config['device']['compare']
   end
 
   def fuzz
     @config['fuzz']
   end
 
-  def capture_page_image (browser, url, width, file_name)
+  def capture_page_image(browser, url, width, file_name)
     puts `"#{browser}" #{@config['phantomjs_options']} "#{snap_file}" "#{url}" "#{width}" "#{file_name}"`
   end
 
-  def compare_images (base, compare, output, info)
+  def compare_images(base, compare, output, info)
     puts `compare -fuzz #{fuzz} -metric AE -highlight-color blue #{base} #{compare} #{output} 2>#{info}`
   end
 
-  def self.crop_images (crop, height)
-    #For compatibility with windows file structures switch commenting on the following 2 lines
+  def self.crop_images(crop, height)
+    # For compatibility with windows file structures switch commenting on the following 2 lines
     puts `convert #{crop} -background none -extent 0x#{height} #{crop}`
-    #puts `convert #{crop.gsub('/', '\\')} -background none -extent 0x#{height} #{crop.gsub('/', '\\')}`
+    # puts `convert #{crop.gsub('/', '\\')} -background none -extent 0x#{height} #{crop.gsub('/', '\\')}`
   end
 
   def crop_images(crop, height)
@@ -82,7 +82,7 @@ class Wraith
   end
 
   def thumbnail_image(png_path, output_path)
-    #For compatibility with windows file structures switch commenting on the following 2 lines
+    # For compatibility with windows file structures switch commenting on the following 2 lines
     `convert #{png_path} -thumbnail 200 -crop 200x200+0+0 #{output_path}`
     #`convert #{png_path.gsub('/', '\\')} -thumbnail 200 -crop 200x200+0+0 #{output_path}`
   end
