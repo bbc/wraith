2899e24dfa187e03f6d6b5dec9ff7944fdac1498
Simon Thulbourn <simon+github@thulbourn.com>
2013-12-30 22:25:12 +0100

    Proper logging
    removes dodgy log outputting from wraith bin to add it correctly to
    wraith's main class.
    
    adds logging to the cli class

--- lib/wraith.rb
+++ lib/wraith.rb
@@ -1,3 +1,23 @@
+require 'log4r'
+
+if ENV['WRAITH_LOG'] && !ENV['WRAITH_LOG'].nil?
+  require 'log4r/config'
+
+  Log4r.define_levels(*Log4r::Log4rConfig::LogLevels)
+
+  begin
+    level = Log4r.const_get(ENV["WRAITH_LOG"].upcase)
+  rescue NameError
+    $stderr.puts "Invalid WRAITH_LOG: #{ENV['WRAITH_LOG']}"
+    exit 1
+  end
+
+  logger = Log4r::Logger.new('wraith')
+  logger.outputters = Log4r::Outputter.stderr
+  logger.level = level
+  logger = nil
+end
+
 module Wraith
   autoload :Environment, 'wraith/environment'
   autoload :CLI, 'wraith/cli'
--- lib/wraith/cli.rb
+++ lib/wraith/cli.rb
@@ -1,6 +1,11 @@
+require 'log4r'
+
 class Wraith::CLI
   def initialize(args, env)
     @env = env
+
+    @logger = Log4r::Logger.new("wraith::cli")
+    @logger.info("CLI: #{args.inspect}")
   end
 
   def start
