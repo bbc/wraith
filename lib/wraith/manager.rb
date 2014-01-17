d507c57a90f25e3971c3fbadc3e1f468f219a9e9
DaveBlooman <david.blooman@gmail.com>
2014-01-17 19:49:19 +0000

    start manager update

--- lib/wraith/manager.rb
+++ lib/wraith/manager.rb
@@ -15,12 +15,12 @@ class WraithManager
   def compare_images
     files = Dir.glob("#{wraith.directory}/*/*.png").sort
 
-    while !files.empty?
+    until !files.empty?
       base, compare = files.slice!(0, 2)
       diff = base.gsub(/([a-z0-9]+).png$/, 'diff.png')
       info = base.gsub(/([a-z0-9]+).png$/, 'data.txt')
       wraith.compare_images(base, compare, diff, info)
-      contents = Dir.glob("#{wraith.directory}/*/*.txt").collect { |f| "\n#{f}\n#{File.read(f)}" }
+      Dir.glob("#{wraith.directory}/*/*.txt").map { |f| "\n#{f}\n#{File.read(f)}" }
       puts 'Saved diff'
     end
   end
@@ -36,39 +36,39 @@ class WraithManager
 
   def save_images
     if !wraith.paths
-      # if there are no path defined in the config use the spider.txt file
-      p = File.read('spider.txt')
-      p = eval(p)
+      paths = File.read('spider.txt')
+      paths = eval(paths)
     else
-      # else use path from config
-      p = wraith.paths
+      paths = wraith.paths
     end
+    path(paths)
+  end
 
-    p.each do |label, path|
+  def path(paths)
+    paths.each do |label, path|
       puts "processing '#{label}' '#{path}'"
-
-      if !path
+      unless path
         path = label
         label = path.gsub('/', '_')
       end
-
       FileUtils.mkdir("#{wraith.directory}/#{label}")
       FileUtils.mkdir_p("#{wraith.directory}/thumbnails/#{label}")
+      capture(label, path)
+    end
+  end
 
-      compare_url = wraith.comp_domain + path if !wraith.comp_domain.nil?
-      base_url = wraith.base_domain + path if !wraith.base_domain.nil?
+  def capture(label, path)
+    compare_url = wraith.comp_domain + path unless wraith.comp_domain.nil?
+    base_url = wraith.base_domain + path unless wraith.base_domain.nil?
 
-      wraith.widths.each do |width|
+    wraith.widths.each do |width|
 
-        wraith.engine.each do |type, engine|
-          # Used for headless browsers
-          compare_file_name = "#{wraith.directory}/#{label}/#{width}_#{engine}_#{wraith.comp_domain_label}.png"
-          base_file_name = "#{wraith.directory}/#{label}/#{width}_#{engine}_#{wraith.base_domain_label}.png"
+      wraith.engine.each do |type, engine|
+        compare_file_name = "#{wraith.directory}/#{label}/#{width}_#{engine}_#{wraith.comp_domain_label}.png"
+        base_file_name = "#{wraith.directory}/#{label}/#{width}_#{engine}_#{wraith.base_domain_label}.png"
 
-          wraith.capture_page_image engine, compare_url, width, compare_file_name if !compare_url.nil?
-          wraith.capture_page_image engine, base_url, width, base_file_name if !base_url.nil?
-
-        end
+        wraith.capture_page_image engine, compare_url, width, compare_file_name unless compare_url.nil?
+        wraith.capture_page_image engine, base_url, width, base_file_name unless base_url.nil?
       end
     end
   end
