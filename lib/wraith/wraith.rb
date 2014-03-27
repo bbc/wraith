1f5ee1917990e183660ece8a2b5f8ee80c05948c
DaveBlooman <david.blooman@gmail.com>
2014-03-27 11:11:33 +0000

    readme

--- lib/wraith/wraith.rb
+++ lib/wraith/wraith.rb
@@ -18,6 +18,7 @@ class Wraith::Wraith
   end
 
   def snap_file
+    puts @snap_path
     file = @config['snap_file'] ? @config['snap_file'] : 'javascript/snap.js'
     File.expand_path(file, @snap_path)
   end
