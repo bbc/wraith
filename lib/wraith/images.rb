fb04e63a2b013ecf72d1744a16979384a368185b
DaveBlooman <david.blooman@gmail.com>
2014-03-28 12:19:17 +0000

    first step

--- lib/wraith/images.rb
+++ lib/wraith/images.rb
@@ -7,14 +7,32 @@ class Wraith::Images
     @wraith = Wraith::Wraith.new(config)
   end
 
+  def invalid
+    File.expand_path('../../assets/invalid.png', File.dirname(__FILE__))
+  end
+
   def files
     files = Dir.glob("#{wraith.directory}/*/*.png").sort
-    invalid = File.expand_path('../../assets/invalid.jpg', File.dirname(__FILE__))
     files.each do |filename|
       if File.stat("#{filename}").size == 0
         FileUtils.cp invalid, filename
         puts "#{filename} is invalid"
       end
+    end
+  end
+
+  def even_images
+    test = Dir["#{wraith.directory}/*/"]
+    test.each do |dir|
+      count = Dir["#{dir}/*"].length
+      is_even?(count, dir)
+    end
+  end
+
+  def is_even?(files, dir)
+    if files.odd?
+      FileUtils.cp invalid, dir
+      puts "An image is missing"
     end
   end
 end
