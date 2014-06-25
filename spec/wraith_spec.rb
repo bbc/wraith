8b09238695fc67b261560d4552ff4a733457f968
DaveBlooman <david.blooman@gmail.com>
2014-06-25 21:41:54 +0100

    specs

--- spec/wraith_spec.rb
+++ spec/wraith_spec.rb
@@ -1,10 +1,10 @@
 require 'rspec-given'
-require 'wraith'
-require 'wraith_manager'
+require 'image_size'
+require './lib/wraith/cli'
 
 describe Wraith do
   Given(:config_name) { 'test_config' }
-  Given { WraithManager.new(config_name).reset_shots_folder }
+  Given { Wraith::FolderManager.new(config_name).clear_shots_folder }
   Given(:test_url1) { 'http://www.live.bbc.co.uk/news' }
   Given(:test_url2) { 'http://www.live.bbc.co.uk/russian' }
   Given { Dir.mkdir('shots/test') }
@@ -13,17 +13,15 @@ describe Wraith do
   Given(:diff_image) { 'shots/test/test_diff.png' }
   Given(:data_txt) { 'shots/test/test.txt' }
 
-  When(:wraith) { Wraith.new(config_name) }
-  Then { wraith.is_a? Wraith }
+  When(:wraith) { Wraith::Wraith.new(config_name) }
+  Then { wraith.is_a? Wraith::Wraith }
 
   context 'When creating a wraith worker' do
-    When(:no_config) { Wraith.new }
-    Then { no_config.should have_failed(ArgumentError) }
 
     Then { wraith.config.keys.size == 8 }
     Then { wraith.widths == [320, 600, 768, 1024, 1280] }
-    Then { wraith.base_domain == 'http://www.live.bbc.co.uk/news' }
-    Then { wraith.comp_domain == 'http://www.live.bbc.co.uk/russian' }
+    Then { wraith.base_domain == 'http://pal.live.bbc.com/news' }
+    Then { wraith.comp_domain == 'http://pal.live.bbc.co.uk/russian' }
     Then { wraith.base_domain_label == 'english' }
     Then { wraith.comp_domain_label == 'russian' }
     Then { wraith.paths == { 'home' => '/', 'uk_index' => '/uk' } }
@@ -46,13 +44,10 @@ describe Wraith do
         wraith.capture_page_image(engine, test_url1, 320, test_image1)
         wraith.capture_page_image(engine, test_url2, 320, test_image2)
       end
-      WraithManager.new(config_name).crop_images
-      wraith.compare_images(test_image1, test_image2, diff_image, data_txt)
+      Wraith::CropImages.new(config_name).crop_images
+      Wraith::CompareImages.new(config_name).compare_task(test_image1, test_image2, diff_image, data_txt)
       ImageSize.path(diff_image).size
     end
     Then { diff_image_size[0] == 320 }
   end
-
-  # crop_images
-  # thumbnail_image
 end
