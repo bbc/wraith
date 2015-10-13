e6ad00ba2dae62527dc26eb3bc9a1e44b14d98da
Marcio Vicente <marciovicente.filho@gmail.com>
2015-10-13 11:44:17 -0300

    Adding more information about failed tests

--- lib/wraith/gallery.rb
+++ lib/wraith/gallery.rb
@@ -130,6 +130,16 @@ class Wraith::GalleryGenerator
       return true
     elsif @failed_shots == false
       puts "Failures detected"
+
+      @dirs.each do |dir, sizes|
+        sizes.to_a.sort.each do |size, files|
+          if files[:data] > wraith.threshold
+            message = "#{dir.gsub('__', '/')} failed in #{size} size resolution"
+            puts "\e[31m#{message}\e[0m"
+          end
+        end
+      end
+
       exit 1
     else
       true
