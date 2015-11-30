24854876a80d092148eaf1c4991f73669c6694cf
ChrisBAshton <chrisashtonweb@gmail.com>
2015-11-30 14:11:15 +0000

    new example configs and fixed #325

--- templates/javascript/beforeCapture--casper_example.js
+++ templates/javascript/beforeCapture--casper_example.js
@@ -1,5 +1,12 @@
-module.exports = function (browserEngine) {
-    browserEngine.wait(2000, function() {
-        browserEngine.click('.ns-panel__hotspot--2');
+module.exports = function (casper) {
+
+    /* you may want to test some interaction on the page */
+    casper.wait(2000, function() {
+        casper.click('.ns-panel__hotspot--2');
     });
+
+    /* or you may want to see how your page looks without JavaScript */
+    // casper.options.pageSettings.javascriptEnabled = false;
+    // casper.thenOpen(casper.page.url);
+
 }
--- templates/javascript/beforeCapture--phantom_example.js
+++ templates/javascript/beforeCapture--phantom_example.js
@@ -1,9 +1,16 @@
-module.exports = function (browserEngine) {
-    browserEngine.evaluate(function(){
+module.exports = function (phantom) {
+
+    /* you may want to test some interaction on the page */
+    phantom.evaluate(function(){
         var a = document.querySelector('.ns-panel__hotspot--2');
         var e = document.createEvent('MouseEvents');
         e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
         a.dispatchEvent(e);
         waitforload = true;
     });
+
+    /* or you may want to see how your page looks without JavaScript */
+    // phantom.settings.javascriptEnabled = false;
+    // phantom.open(phantom.page.url);
+
 }
