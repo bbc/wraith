24b04c341259475aa01d42731b0ab837f7f3836c
ChrisBAshton <chrisashtonweb@gmail.com>
2015-12-17 12:02:37 +0000

    added skeleton code spec for cli args

---
+++ spec/construct_command_spec.rb
@@ -0,0 +1,9 @@
+require "_helpers"
+
+describe "Wraith config to CLI argument mapping" do
+  describe "construct_command" do
+    it "passing variables to construct_command outputs correct command" do
+
+    end
+  end
+end
