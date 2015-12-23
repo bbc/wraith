e63e25c31391c586d2696b1f676a5e8f6eb4ed63
ChrisBAshton <chrisashtonweb@gmail.com>
2015-12-23 11:56:06 +0000

    added the default phantom arguments to the tests

--- spec/construct_command_spec.rb
+++ spec/construct_command_spec.rb
@@ -13,7 +13,7 @@ describe "Wraith config to CLI argument mapping" do
       global_before_capture = 'javascript/before_capture.js'
       path_before_capture = 'false'
       command = saving.construct_command(width, url, file_name, selector, global_before_capture, path_before_capture)
-      expect(command).to eq "phantomjs  '/Users/ashton/Sites/bbc/wraith/lib/wraith/javascript/phantom.js' 'http://example.com/my-page' '320' 'wraith/my-page/320_phantomjs_latest.png' '.my_selector' 'javascript/before_capture.js' 'false'"
+      expect(command).to eq "phantomjs --ignore-ssl-errors=true --ssl-protocol=tlsv1 '/Users/ashton/Sites/bbc/wraith/lib/wraith/javascript/phantom.js' 'http://example.com/my-page' '320' 'wraith/my-page/320_phantomjs_latest.png' '.my_selector' 'javascript/before_capture.js' 'false'"
       end
 
     it "should allow hashtags in selectors" do
@@ -24,7 +24,7 @@ describe "Wraith config to CLI argument mapping" do
       global_before_capture = 'javascript/before_capture.js'
       path_before_capture = 'false'
       command = saving.construct_command(width, url, file_name, selector, global_before_capture, path_before_capture)
-      expect(command).to eq "phantomjs  '/Users/ashton/Sites/bbc/wraith/lib/wraith/javascript/phantom.js' 'http://example.com/my-page' '320' 'wraith/my-page/320_phantomjs_latest.png' '\\#some-id' 'javascript/before_capture.js' 'false'"
+      expect(command).to eq "phantomjs --ignore-ssl-errors=true --ssl-protocol=tlsv1 '/Users/ashton/Sites/bbc/wraith/lib/wraith/javascript/phantom.js' 'http://example.com/my-page' '320' 'wraith/my-page/320_phantomjs_latest.png' '\\#some-id' 'javascript/before_capture.js' 'false'"
     end
 
     it "should be able to pass multiple widths at once" do
@@ -35,7 +35,7 @@ describe "Wraith config to CLI argument mapping" do
       global_before_capture = 'javascript/before_capture.js'
       path_before_capture = 'false'
       command = saving.construct_command(width, url, file_name, selector, global_before_capture, path_before_capture)
-      expect(command).to eq "phantomjs  '/Users/ashton/Sites/bbc/wraith/lib/wraith/javascript/phantom.js' 'http://example.com/my-page' '320,624,976' 'wraith/my-page/320_phantomjs_latest.png' '.my_selector' 'javascript/before_capture.js' 'false'"
+      expect(command).to eq "phantomjs --ignore-ssl-errors=true --ssl-protocol=tlsv1 '/Users/ashton/Sites/bbc/wraith/lib/wraith/javascript/phantom.js' 'http://example.com/my-page' '320,624,976' 'wraith/my-page/320_phantomjs_latest.png' '.my_selector' 'javascript/before_capture.js' 'false'"
     end
   end
 end
