7d510331a781c6c7da3167e66b9871f02fb62a24
ChrisBAshton <chrisashtonweb@gmail.com>
2016-02-28 14:30:11 +0000

    fix #369

--- lib/wraith/helpers/utilities.rb
+++ lib/wraith/helpers/utilities.rb
@@ -6,6 +6,9 @@ def convert_to_absolute(filepath)
   elsif filepath[0] == "/"
     # filepath is already absolute. return unchanged
     filepath
+  elsif filepath.match(/^[A-Z]:\/(.+)$/)
+    # filepath is an absolute Windows path, e.g. C:/Code/Wraith/javascript/global.js. return unchanged
+    filepath
   else
     # filepath is relative. it must be converted to absolute
     "#{Dir.pwd}/#{filepath}"
