f4b91bbf0d6aaddcb9c3b757b5efa676db984c18
Maxim Ivanov <mivanov1@gmail.com>
2017-10-06 11:16:56 -0400

    resize after js execution because page might change size

--- lib/wraith/save_images.rb
+++ lib/wraith/save_images.rb
@@ -125,9 +125,9 @@ class Wraith::SaveImages
       new_file_name = file_name.sub('MULTI', screen_size)
       driver = get_driver screen_size
       driver.navigate.to url
-      resize_to_fit_page(driver) unless screen_size['x']
       driver.execute_async_script(File.read(global_before_capture)) if global_before_capture
       driver.execute_async_script(File.read(path_before_capture)) if path_before_capture
+      resize_to_fit_page(driver) unless screen_size['x']
       driver.save_screenshot(new_file_name)
       crop_selector(driver, selector, new_file_name) if selector && selector.length > 0
       driver.quit
